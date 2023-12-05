d1b62328e6535ba0c0738e6c2ed312e6c27a3fad
ptichka-tanya <t.n.sinitsina@gmail.com>
2023-12-05 15:37:24 +0300

    add function upper_text

--- str_func.py
+++ str_func.py
@@ -1,7 +1,18 @@
+"""Функия, которая делает текст большим"""
+
+
 def upper_text(text):
     return text.upper()
+
+
+"""Функция, которая делает первые буквы слов заглавными"""
+
+
+def title_word(text):
+    return text.title()
 
 
 user_text = input()
 
 print(upper_text(user_text))
+print(title_word(user_text))
